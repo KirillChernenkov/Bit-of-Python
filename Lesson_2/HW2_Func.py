e8057ea7b3ed23ac6bb991872ce0717d390ed7b1
KirillChernenkov <chernenkov.kir@mail.ru>
2022-01-28 22:21:00 +0400

    HW2

--- Lesson_2/HW2_Func.py
+++ Lesson_2/HW2_Func.py
@@ -1,6 +1,30 @@
 #TASK1
-def new(x:int(),y:int()):
-    return x+y
+#1.	Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+# Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+# Использовать два варианта обработки деления на 0. Через if else и через try except
 
-a,b = int(input()), int(input())
-print(new(a,b))
+def new(x:int(),y:int()):
+    while True:
+        try:
+            return x/y
+        except ValueError:
+            print('Ошибка. Ожидалось вещественное число.')
+
+
+
+#a,b = int(input()), int(input())
+#print(new(a,b))
+
+#TASK3
+#3.	Реализовать функцию my_func(), которая принимает три позиционных аргумента и возвращает сумму наибольших двух аргументов.
+# Обработать условие ввода строки
+# Нельзя использовать функции max() min()
+
+def my_func():
+    a,b,c = [input() for _ in range(3)]
+    if not a.isnumeric() or not b.isnumeric() or not c.isnumeric():
+        print('Неправильный ввод')
+    if (a>b)and(a>c)and(b>c):
+        return print(int(a)+int(b))
+
+my_func()
