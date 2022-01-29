783fd2849a597594be50144c9f92dc1e62062559
KirillChernenkov <chernenkov.kir@mail.ru>
2022-01-29 22:37:06 +0400

    HW2

--- Lesson_2/HW2_Func.py
+++ Lesson_2/HW2_Func.py
@@ -133,8 +133,8 @@ def make_readable(time_sec):
     else:
         return print(round(h),':',round(m), ':',round(s))
 
-sec = int(input())
-make_readable(sec)
+#sec = int(input())
+#make_readable(sec)
 
 #TASK8
 #8.	Функция max_sequence() принимает массив (позиционный аргумент) заполненный рандомными числами в диапазоне от -100 до 100.
@@ -143,5 +143,26 @@ make_readable(sec)
 #Функция возвращает строку «Положительные победили» , если сумма положительных элементов больше чем  абсолютная сумма отрицательных элементов
 # и «Отрицательные победили»  если  наоборот. Так же обработать возможность равной суммы и вывести «Все равны»
 
-def max_sequence():
-    return 1
+from random import randint
+
+def max_sequence(n):
+    a = []
+    sum_pos = int()
+    sum_neg = int()
+    for i in range(0, n):
+        a.insert(i, randint(-100, 100))
+    print(*a)
+    for i in range(0,n):
+        if a[i]>0:
+            sum_pos+=a[i]
+        else:
+            sum_neg+=a[i]
+    if sum_pos>abs(sum_neg):
+        return print("Положительные победили")
+    else:
+        return print("Отрицательные победили")
+
+aa = int(input())
+max_sequence(aa)
+
+
