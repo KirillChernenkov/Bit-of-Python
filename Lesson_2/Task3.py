9f22bb6c64617a64f65c5537f9eada396262235a
KirillChernenkov <chernenkov.kir@mail.ru>
2022-01-31 18:36:39 +0400

    Task3

--- Lesson_2/Task3.py
+++ Lesson_2/Task3.py
@@ -4,15 +4,15 @@
 
 # Нельзя использовать функции max() min()
 
-def my_func():
+def my_func(a,b,c):
     def maxx(a:int,b:int):
         if a>b:
             return a
         else:
             return b
-    a,b,c = [input() for _ in range(3)]
     if not a.isnumeric() or not b.isnumeric() or not c.isnumeric():
         print('Неправильный ввод')
     return print(int(maxx(a,b))+int(maxx(b,c)))
 
-my_func()
+a1,b1,c1 = [input() for _ in range(3)]
+my_func(a1,b1,c1)
