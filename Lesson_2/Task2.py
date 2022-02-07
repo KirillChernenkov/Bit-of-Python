a4ad5384dd68af0aaa538b17bf404661ec601ae7
KirillChernenkov <92977321+KirillChernenkov@users.noreply.github.com>
2022-02-07 21:15:59 +0300

    Update Task2.py

--- Lesson_2/Task2.py
+++ Lesson_2/Task2.py
@@ -7,4 +7,4 @@ def info(name,surname,birthday,city,email,tel):
     return print(f'Здравствуйте,{name} {surname}, дата рождения {birthday}, проживающий в городе {city}, email {email}, телефон {tel}')
 
 name_input,surname_input,birthday_input,city_input,email_input,tel_input = [input() for _ in range(6)]
-info(name_input,surname_input,birthday_input,city_input,email_input,tel_input)
+info(name=name_input,surname=surname_input,birthday=birthday_input,city=city_input,email=email_input,tel=tel_input)
