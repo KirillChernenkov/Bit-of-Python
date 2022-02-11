518a63ac5e6f475342962615791ec1dac8c0bb3b
Admin <chernenkov.kir@mail.ru>
2022-02-11 18:53:31 +0400

    1

--- HW_Files/HW.py
+++ HW_Files/HW.py
@@ -3,6 +3,10 @@ import os
 #os.path.isdir("C:\Users\Admin\PycharmProjects\Bit-of-Python\HW_Files")
 b = open(r"C:\Users\Admin\PycharmProjects\Bit-of-Python\HW_Files\task1.txt","w+")
 print(b.name)
-b.write('HEllO')
-b.write('HEllO')
+c = str()
+while True:
+    c = input()
+    if c == '': break
+    b.write(c)
+
 b.close()
