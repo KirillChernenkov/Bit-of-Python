4d2bcbaa933e972679a25887e86affd888a37748
KirillChernenkov <92977321+KirillChernenkov@users.noreply.github.com>
2022-02-17 19:32:01 +0400

    Update Task4.py

--- Files/Task4.py
+++ Files/Task4.py
@@ -1,6 +1,4 @@
-with open(r'C:\Users\Admin\PycharmProjects\Bit-of-Python\HW_Files\task4.txt','w+') as file_4:
-    line = input()
-    file_4.writelines(line)
-    my_numb = line.split()
-
-print(sum(map(int, my_numb)))
+with open(r'task4.txt','w+') as file_4:
+    digits = [12121,2222,3,4,511]    
+    bb = sum(digits)
+    file_4.writelines(str(bb))  
